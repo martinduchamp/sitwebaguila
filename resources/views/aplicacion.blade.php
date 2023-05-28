97e4afc8e44b780b787518e1ef092a0e3f52a064
Abiel Magu <abielmagu.dev@gmail.com>
2023-05-28 03:49:03 -0500

    Actualiza estilo de bootstrap design

--- resources/views/aplicacion.blade.php
+++ resources/views/aplicacion.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name') }}</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.3/united/bootstrap.min.css" integrity="sha512-OcJVUFAaQ1kaFvQN/nsIoTSTdCyF7aVSpNv5BkqRm9DvMCM+9hndo+Lz8mfZ9Q6tPOn+tSXjZT3g+CSzFeUArA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
     <body>
         @include('aplicacion.navbar')
