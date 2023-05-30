e19763cf84c1a7e011c49e46734a5a1b5300005c
Abiel Magu <abielmagu.dev@gmail.com>
2023-05-30 00:31:52 -0500

    Actualiza views

--- resources/views/aplicacion/navbar.blade.php
+++ resources/views/aplicacion/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="#"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -27,15 +27,9 @@
               <a class="dropdown-item" href="{{ route('tipos_remolque.index') }}">
                 <span>Tipos de remolque</span>
               </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Separated link</a>
             </div>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-sm-2" type="search" placeholder="Search">
-          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
--- resources/views/movimientos/index.blade.php
+++ resources/views/movimientos/index.blade.php
@@ -8,39 +8,38 @@
     <table class="table table-hover align-middle">
         <thead>
             <tr>
+                <th></th>
                 <th>Número</th>
                 <th>Placas</th>
                 <th>Propietario</th>
                 <th>Tipo</th>
                 <th class='text-nowrap'>Línea americana</th>
-                <th class='text-nowrap'>Cerrado en extranjero</th>
-                <th class='text-nowrap'>Cruce directo</th>
                 <th>Origen</th>
                 <th>Destino</th>
                 <th class='text-nowrap'>Dias en patio</th>
+                <th class='text-nowrap'>Cerrado</th>
+                <th class='text-nowrap'>Cruce</th>
                 <th class='text-nowrap'>Fecha y hora</th>
-                <th></th>
             </tr>
         </thead>
         <tbody>
             @foreach($movimientos as $movimiento)
             <tr>
-                <td>{{ $movimiento->numero_remolque }}</td>
-                <td>{{ $movimiento->placas_remolque }}</td>
-                <td>{{ $movimiento->propietario_remolque }}</td>
-                <td>{{ $movimiento->tipoRemolque->nombre }}</td>
-                <td>{{ $movimiento->lineaAmericana->nombre }}</td>
-                <td class='text-capitalize'>{{ $movimiento->responde_es_cerrado_extranjero }}</td>
-                <td class='text-capitalize'>{{ $movimiento->responde_es_cruce_directo }}</td>
-                <tdtd>
+                <td class="text-end">
+                    <a href="{{ route('movimientos.edit', $movimiento) }}" class="btn btn-warning btn-sm">Editar</a>
+                </td>
+                <td class='text-nowrap'>{{ $movimiento->numero_remolque }}</td>
+                <td class='text-nowrap'>{{ $movimiento->placas_remolque }}</td>
+                <td class='text-nowrap'>{{ $movimiento->propietario_remolque }}</td>
+                <td class='text-nowrap'>{{ $movimiento->tipoRemolque->nombre }}</td>
+                <td class='text-nowrap'>{{ $movimiento->lineaAmericana->nombre }}</td>
                 <td></td>
                 <td></td>
                 <td>{{ $movimiento->cantidad_dias_patio }}</td>
-                <td>
+                <td class='text-capitalize'>{{ $movimiento->responde_es_cerrado_extranjero }}</td>
+                <td class='text-capitalize'>{{ $movimiento->responde_es_cruce_directo }}</td>
+                <td class='text-nowrap'>
                     {{ $movimiento->fecha }} {{ $movimiento->hora }}
-                </td>
-                <td class="text-end">
-                    <a href="{{ route('movimientos.edit', $movimiento) }}" class="btn btn-warning btn-sm">Editar</a>
                 </td>
             </tr>
             @endforeach
